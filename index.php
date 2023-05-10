1a14328923b44ef1add9815b345f317529a31fbe
rangapin <rangapin@gmx.com>
2023-05-10 23:41:35 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+
+$words = file("words.txt");
+
+$word = rtrim($words[array_rand($words)]);
+
+echo $word;
+echo "<br>";
+
+$wordLength = strlen($word);
+echo $wordLength;
+echo "<br>";
+
+$guesses = ["a", "e"];
+
+for($i = 0; $i < $wordLength; $i++){
+    if(in_array($word[$i], $guesses)){
+        echo $word[$i];
+    } else {
+        echo "_";
+    }
+    echo " ";
+}
